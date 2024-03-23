eee6efe5de9a151878d1a35288d951cb670a0719
maffinyep <marvin@ranieri>
2024-03-23 14:51:58 +0100

    fix engine on env var

--- backend/app/src/dbsession.py
+++ backend/app/src/dbsession.py
@@ -1,8 +1,18 @@
-from sqlalchemy import create_engine
+import os
+
+from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:admin@localhost:5433/postgres"
+# TODO remove default values and raise ImportError
+url_object = URL.create(
+    drivername="postgresql+psycopg",
+    username=os.environ["PGUSER"],
+    password=os.environ["PGPASSWORD"],
+    host=os.environ["PGHOST"],
+    port=int(os.environ["PGPORT"]),
+    database=os.environ["PGDATABASE"],
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(url_object)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
