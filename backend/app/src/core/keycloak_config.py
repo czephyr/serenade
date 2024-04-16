49a8c99c91e2bbfd0dc35cac7e886f651805297b
czephyr <alfredo.funicello@studenti.unimi.it>
2024-04-16 20:07:18 +0200

    backend keycloak env vars fixed

--- backend/app/src/core/keycloak_config.py
+++ backend/app/src/core/keycloak_config.py
@@ -3,7 +3,7 @@ from keycloak import KeycloakOpenID
 from fastapi.security import OAuth2PasswordBearer
 
 keycloak_openid = KeycloakOpenID(
-    server_url=os.getenv("KEYCLOAK_URL"),
+    server_url=os.getenv("KEYCLOAK_HOST"),
     client_id=os.getenv("KEYCLOAK_CLIENT_ID"),
     realm_name=os.getenv("KEYCLOAK_REALM"),
     client_secret_key=os.getenv("KEYCLOAK_CLIENT_SECRET"),
