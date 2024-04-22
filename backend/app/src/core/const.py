236b77035f62f88f86ed8ece52218b31049e743b
maffinyep <marvin@ranieri>
2024-04-23 00:33:12 +0200

    minor changes

--- backend/app/src/core/const.py
+++ backend/app/src/core/const.py
@@ -2,4 +2,4 @@ import os
 
 SMS_PATIENT_CREATE = """Nuova installazion da effettuare, buon lavoro!"""
 ADMIN_USERNAME = "~serenade"
-SALT_HASH = bytes.fromhex(os.environ.get("SALT_HASH", ""))
+SALT_HASH = bytes.fromhex(os.getenv("SALT_HASH", ""))
