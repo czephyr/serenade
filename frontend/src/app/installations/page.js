d82b5ed4d4b6a8c76a214f45a9131ffaa6cf69bc
czephyr <alfredo.funicello@studenti.unimi.it>
2024-04-24 20:07:54 +0200

    styling list pages

--- frontend/src/app/installations/page.js
+++ frontend/src/app/installations/page.js
@@ -2,7 +2,7 @@ import { getServerSession } from "next-auth";
 import authOptions from "../api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
 import { getAccessToken } from "../../utils/sessionTokenAccessor";
-import { SetDynamicRoute } from "@/utils/setDynamicRoute";
+import StatusBadge from "../../components/statusBadge"; // Assume this is your form component
 
 async function getAllInstallations() {
   const url = `${process.env.BACKEND_HOST}/api/v1/installations/`; // Adjust the URL to your tickets API endpoint
@@ -36,10 +36,11 @@ export default async function Installations() {
       const installations = await getAllInstallations(); // Fetch tickets instead of patients
 
       return (
-        <main className="text-white">
-          <SetDynamicRoute></SetDynamicRoute>
-          <main>
-            <h1 className="text-4xl text-center">Installation List</h1>
+        <main className="bg-gray-100 min-h-screen pt-10 pb-6 px-2 md:px-0">
+          <div className="max-w-3xl mx-auto px-4 bg-white shadow rounded-lg p-6">
+            <h1 className="text-2xl font-bold text-center mb-2">
+              Installation List
+            </h1>
             <div className="overflow-x-auto">
               <table className="min-w-full leading-normal">
                 <thead>
@@ -51,10 +52,7 @@ export default async function Installations() {
                       Status
                     </th>
                     <th className="px-5 py-3 border-b-2 text-left text-xs font-semibold uppercase tracking-wider">
-                      Last Update
-                    </th>
-                    <th className="px-5 py-3 border-b-2 text-left text-xs font-semibold uppercase tracking-wider">
-                      Color
+                      Ultimo Update
                     </th>
                     <th className="px-5 py-3 border-b-2 text-left text-xs font-semibold uppercase tracking-wider">
                       Action
@@ -65,21 +63,34 @@ export default async function Installations() {
                   {installations.map((installation) => (
                     <tr key={installation.patient_id}>
                       <td className="px-5 py-5 border-b">
-                        Patient {installation.patient_id}
+                        {installation.patient_id}
                       </td>
                       <td className="px-5 py-5 border-b">
-                        {installation.status}
+                        <StatusBadge status={installation.status} />
                       </td>
                       <td className="px-5 py-5 border-b">
                         {installation.date_delta}
                       </td>
-                      <td className="px-5 py-5 border-b">{installation.hue}</td>
                       <td className="px-5 py-5 border-b">
                         <a
                           href={`/installations/${installation.patient_id}`}
-                          className="text-blue-500 hover:underline"
+                          className="inline-flex items-center justify-center bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                          role="button"
                         >
-                          View Details
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z"
+                            />
+                          </svg>
                         </a>
                       </td>
                     </tr>
@@ -87,8 +98,7 @@ export default async function Installations() {
                 </tbody>
               </table>
             </div>
-          </main>
-          <div>{JSON.stringify(installations)}</div>
+          </div>
         </main>
       );
     } catch (err) {
