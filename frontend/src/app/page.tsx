11c36b4ea871d3837ed891eef9b0ccba682a2aa0
czephyr <alfredo.funicello@studenti.unimi.it>
2024-05-01 15:20:42 +0200

    added redirect on home if logged in

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,12 @@
 import AuthStatus from "../components/authStatus"
-export default function Home() {
+import { getServerSession } from "next-auth";
+import authOptions from "./api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if(session){
+    redirect("/redirect")
+  }
   return (
     <main className="bg-gray-100 min-h-screen pt-10 pb-6 px-2 md:px-0">
       <div className="max-w-md mx-auto px-4 bg-white shadow rounded-lg p-6">
